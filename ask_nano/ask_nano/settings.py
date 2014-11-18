0a588c131175a8fc63a9984fc466640d8bf269c2
nanomishka <mihail777str@yandex.ru>
2014-11-19 02:38:49 +0300

    static_path+div_row

--- ask_nano/ask_nano/settings.py
+++ ask_nano/ask_nano/settings.py
@@ -12,6 +12,8 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
+k = BASE_DIR
+
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
@@ -25,7 +27,7 @@ DEBUG = True
 TEMPLATE_DEBUG = True
 
 TEMPLATE_DIRS = (
-    '/home/nano/web/ask_nano/templates',
+    os.path.join(BASE_DIR, "templates"),
 )
 
 ALLOWED_HOSTS = []
@@ -86,4 +88,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = "/static/"
+
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+    BASE_DIR,
+)
